8de0b33960c31b890f4bdd6205dd0abe9390b888
Mohammed amine ztait <Mztait@wil.yearup.org>
2023-03-31 15:50:37 -0700

    Initial commit

---
+++ src/Salary.java
@@ -0,0 +1,26 @@
+public class Salary extends employee2  {
+private double time;
+private double Salary;
+
+public salary
+    public double getTime(){
+
+        return time;
+    }
+    public void setTime (double time){
+        this.time = time;
+
+
+    }
+    public double getSalary(){
+
+        return Salary;
+    }
+    public void setSalary (){
+       Salary = getTime() * getWage();
+
+
+    }
+
+
+}
